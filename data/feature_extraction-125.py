7afb058a11db7beade43949f2e484b74ceb28dba
Ramona Rajagopalan <65932912+flinksey@users.noreply.github.com>
2023-05-15 20:44:15 +0800

    Add files via upload

---
+++ data/feature_extraction-125.py
@@ -0,0 +1,313 @@
+import numpy as np
+import scipy as sp
+from scipy import stats
+from scipy import signal
+from numpy.lib.format import open_memmap
+
+train_windows = np.load("1-point-25-sec/train_windows.npy", mmap_mode='r')
+test_windows = np.load("1-point-25-sec/test_windows.npy", mmap_mode='r')
+
+train_features = open_memmap('1-point-25-sec/train_features.npy', mode='w+', dtype=np.float32, shape=(train_windows.shape[0], 161))
+test_features = open_memmap('1-point-25-sec/test_features.npy', mode='w+', dtype=np.float32, shape=(test_windows.shape[0], 161))
+
+fs = 62 # ~1.25s window size
+
+def corr_grav(x,y):
+    corr = np.corrcoef(x,y)[0][1]
+    if np.isnan(corr):
+          corr = 0
+    return corr
+
+def cv_gravity(data):
+        cv = stats.variation(data)
+        if np.isnan(cv):
+          cv = 0  # some might return NaN bc mean and std are 0
+        return cv
+
+def freq_cent(fft_ampl, fft_freq):
+        sums = np.sum(fft_ampl)
+        sums = np.where(sums, sums, 1.)  # Avoid dividing by zero
+        return np.sum(fft_freq * fft_ampl) / sums
+
+# GENERATE TRAIN FEATURES
+i = 0 # counter
+for window in train_windows:
+    window_features = []
+    # print(f"window shape: {window.shape}")
+
+    b_mag = (np.linalg.norm(window[:3,:],axis=0))
+    t_mag = (np.linalg.norm(window[3:,:],axis=0))
+    # print(f"b_mag shape: {b_mag.shape}")
+
+    b_mag = b_mag.reshape(list(b_mag.shape)+[1])
+    t_mag = t_mag.reshape(list(t_mag.shape)+[1])
+    # print(f"b_mag reshape: {b_mag.shape}")
+
+    window = np.transpose(window)
+    # print(f"window transpose: {window.shape}")
+
+    window_withMag = np.concatenate([window,b_mag,t_mag], axis=1)
+    # print(f"window_withMag shape: {window_withMag.shape}")
+    
+    window_withMag = np.transpose(window_withMag)
+    # print(f"window_withMag transpose: {window_withMag.shape}")
+
+    #  Filtering
+    # create the LP filter
+    # 1Hz 4th order LP Butterworth filter
+    b, a  = signal.butter(4, Wn=1, fs=fs, btype='lowpass')
+
+    # create the HP filter
+    # 1Hz 4th order HP Butterworth filter
+    d, c = signal.butter(4, Wn=1, fs=fs, btype='highpass')
+
+    window_grav = signal.filtfilt(b, a, window_withMag, axis=1)
+    window_mot = signal.filtfilt(d, c, window_withMag, axis=1)
+
+    #print(len(window_features))
+    # kept these as .append instead of += bc when I tested it, it threw an error when including the means later on
+    window_features.append(corr_grav(window_withMag[0],window_withMag[1]))
+    #print(corr_grav(window_withMag[0],window_withMag[1]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[2]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[1],window_withMag[2]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[2],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[2],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[2],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[3],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[3],window_withMag[5]))
+    window_features.append(corr_grav(window_withMag[4],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[6],window_withMag[7]))
+
+    #print(len(window_features))
+
+
+    # GRAVITY:
+    #  Mean over all axes
+    #print(np.concatenate(window_grav[0]).mean())
+    window_features.append(np.mean(window_grav))
+    #print(len(window_features))
+
+    # Mean
+    window_features+=[np.mean(x) for x in window_grav]
+
+    # Median
+    window_features+=[np.median(x) for x in window_grav]
+
+    # Standard Deviation
+    window_features+=[np.std(x) for x in window_grav]
+
+    #  Coefficient of Variation
+    window_features+=[cv_gravity(x) for x in window_grav]
+    
+    #  Min Value
+    window_features+=[np.min(x) for x in window_grav]
+
+    # 25th Percentile
+    window_features+=[np.percentile(x,25) for x in window_grav]
+
+    # 50th Percentile
+    window_features+=[np.percentile(x,50) for x in window_grav]
+
+    # 75th Percentile
+    window_features+=[np.percentile(x,75) for x in window_grav]
+
+    # Max Value
+    window_features+=[np.max(x) for x in window_grav]
+
+    # ACCELERATION:
+    # Skew
+    window_features+=[stats.skew(x) for x in window_mot]
+
+    # Kurtosis
+    window_features+=[stats.kurtosis(x) for x in window_mot]
+
+    # Signal Energy
+    window_features+=[np.sum(x**2) for x in window_mot]
+
+    # Frequency-Domain Mean
+    # FFT
+    window_fft = np.abs(np.fft.fft(window_withMag, axis=0))
+
+    window_features+=[np.mean(x) for x in window_fft]
+
+    # Frequency-Domain Standard Deviation
+    window_features+=[np.std(x) for x in window_fft]
+
+    # Dominant Frequency
+    window_features+=[np.fft.fftfreq(window.shape[0])[x.argmax(axis=0)] for x in window_fft]
+
+    # Dominant Frequency Magnitude
+    window_features+=[np.max(x) for x in window_fft]
+
+    # Spectral Centroid
+    window_features+=[freq_cent(x, np.fft.fftfreq(window.shape[0])) for x in window_fft]
+    
+    # Total Signal Power
+    window_features+=[np.sum(x**2) for x in window_fft]
+    
+    train_features[i] = window_features
+
+    i += 1
+    print(f"train window: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    
+    # if i == 1:
+    #   print(f"window: {i}/{len(train_windows)}, feature len: {len(window_features)}") 
+    #   print(f"window features: ")
+    #   print(window_features)
+    #   print(f"train features: ")
+    #   print(train_features)
+    # if i == 100:
+    #   print(f"\nwindow: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    #   print(train_features)
+    # if i == 500:
+    #   print(f"\nwindow: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    # if i == 1000:
+    #   print(f"\nwindow: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    # if i == 10000:
+    #   print(f"\nwindow: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    # if i == 50000:
+    #   print(f"\nwindow: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    # if i == 100000:
+    #   print(f"window: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+    #   #break
+    # if i == 1000000:
+    #   print(f"window: {i}/{len(train_windows)}, feature len: {len(window_features)}")
+
+# GENERATE TEST FEATURES
+i=0
+for window in test_windows:
+    window_features = []
+
+    b_mag = (np.linalg.norm(window[:3,:],axis=0))
+    t_mag = (np.linalg.norm(window[3:,:],axis=0))
+
+    b_mag = b_mag.reshape(list(b_mag.shape)+[1])
+    t_mag = t_mag.reshape(list(t_mag.shape)+[1])
+
+    window = np.transpose(window)
+
+    window_withMag = np.concatenate([window,b_mag,t_mag], axis=1)
+    
+    window_withMag = np.transpose(window_withMag)
+
+    #  Filtering
+    # create the LP filter
+    # 1Hz 4th order LP Butterworth filter
+    b, a  = signal.butter(4, Wn=1, fs=fs, btype='lowpass')
+
+    # create the HP filter
+    # 1Hz 4th order HP Butterworth filter
+    d, c = signal.butter(4, Wn=1, fs=fs, btype='highpass')
+
+    window_grav = signal.filtfilt(b, a, window_withMag, axis=1)
+    window_mot = signal.filtfilt(d, c, window_withMag, axis=1)
+
+    #print(len(window_features))
+    # kept these as .append instead of += bc when I tested it, it threw an error when including the means later on
+    window_features.append(corr_grav(window_withMag[0],window_withMag[1]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[2]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[0],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[1],window_withMag[2]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[1],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[2],window_withMag[3]))
+    window_features.append(corr_grav(window_withMag[2],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[2],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[3],window_withMag[4]))
+    window_features.append(corr_grav(window_withMag[3],window_withMag[5]))
+    window_features.append(corr_grav(window_withMag[4],window_withMag[5]))
+
+    window_features.append(corr_grav(window_withMag[6],window_withMag[7]))
+
+    # GRAVITY:
+    #  Mean over all axes
+    window_features.append(np.mean(window_grav))
+
+    # Mean
+    window_features+=[np.mean(x) for x in window_grav]
+
+    # Median
+    window_features+=[np.median(x) for x in window_grav]
+
+    # Standard Deviation
+    window_features+=[np.std(x) for x in window_grav]
+
+    #  Coefficient of Variation
+    window_features+=[cv_gravity(x) for x in window_grav]
+    
+    #  Min Value
+    window_features+=[np.min(x) for x in window_grav]
+
+    # 25th Percentile
+    window_features+=[np.percentile(x,25) for x in window_grav]
+
+    # 50th Percentile
+    window_features+=[np.percentile(x,50) for x in window_grav]
+
+    # 75th Percentile
+    window_features+=[np.percentile(x,75) for x in window_grav]
+
+    # Max Value
+    window_features+=[np.max(x) for x in window_grav]
+
+    # ACCELERATION:
+    # Skew
+    window_features+=[stats.skew(x) for x in window_mot]
+
+    # Kurtosis
+    window_features+=[stats.kurtosis(x) for x in window_mot]
+
+    # Signal Energy
+    window_features+=[np.sum(x**2) for x in window_mot]
+
+    # Frequency-Domain Mean
+    # FFT
+    window_fft = np.abs(np.fft.fft(window_withMag, axis=0))
+
+    window_features+=[np.mean(x) for x in window_fft]
+
+    # Frequency-Domain Standard Deviation
+    window_features+=[np.std(x) for x in window_fft]
+
+    # Dominant Frequency
+    window_features+=[np.fft.fftfreq(window.shape[0])[x.argmax(axis=0)] for x in window_fft]
+
+    # Dominant Frequency Magnitude
+    window_features+=[np.max(x) for x in window_fft]
+
+    # Spectral Centroid
+    window_features+=[freq_cent(x, np.fft.fftfreq(window.shape[0])) for x in window_fft]
+    
+    # Total Signal Power
+    window_features+=[np.sum(x**2) for x in window_fft]
+    
+    test_features[i] = window_features
+    
+    i+=1
+    print(f"test window: {i}/{len(test_windows)}, feature len: {len(window_features)}")
+
+
+    
+# Replace NaN values with 0
+train_features[np.isnan(train_features)] = 0
+test_features[np.isnan(test_features)] = 0
+
+# SAVE FEATURES
+train_features.flush()
+test_features.flush()
---
+++ data/process_data-125.py
@@ -0,0 +1,167 @@
+import random
+import json
+
+import pandas as pd
+import numpy as np
+import scipy as sp
+from numpy.lib.stride_tricks import sliding_window_view
+from numpy.lib.format import open_memmap
+from keras.utils.np_utils import to_categorical
+
+num_samples = 62 # ~1.25s
+
+harth_filenames = [
+    './harth-ml-experiments/harth/S006.csv',
+    './harth-ml-experiments/harth/S008.csv',
+    './harth-ml-experiments/harth/S009.csv',
+    './harth-ml-experiments/harth/S010.csv',
+    './harth-ml-experiments/harth/S012.csv',
+    './harth-ml-experiments/harth/S013.csv',
+    './harth-ml-experiments/harth/S014.csv',
+    './harth-ml-experiments/harth/S015.csv',
+    './harth-ml-experiments/harth/S016.csv',
+    './harth-ml-experiments/harth/S017.csv',
+    './harth-ml-experiments/harth/S018.csv',
+    './harth-ml-experiments/harth/S019.csv',
+    './harth-ml-experiments/harth/S020.csv',
+    './harth-ml-experiments/harth/S021.csv',
+    './harth-ml-experiments/harth/S022.csv',
+    './harth-ml-experiments/harth/S023.csv',
+    './harth-ml-experiments/harth/S024.csv',
+    './harth-ml-experiments/harth/S025.csv',
+    './harth-ml-experiments/harth/S026.csv',
+    './harth-ml-experiments/harth/S027.csv',
+    './harth-ml-experiments/harth/S028.csv',
+    './harth-ml-experiments/harth/S029.csv',
+    './harth-ml-experiments/har70plus/501.csv',
+    './harth-ml-experiments/har70plus/502.csv',
+    './harth-ml-experiments/har70plus/503.csv',
+    './harth-ml-experiments/har70plus/504.csv',
+    './harth-ml-experiments/har70plus/505.csv',
+    './harth-ml-experiments/har70plus/506.csv',
+    './harth-ml-experiments/har70plus/507.csv',
+    './harth-ml-experiments/har70plus/508.csv',
+    './harth-ml-experiments/har70plus/509.csv',
+    './harth-ml-experiments/har70plus/510.csv',
+    './harth-ml-experiments/har70plus/511.csv',
+    './harth-ml-experiments/har70plus/512.csv',
+    './harth-ml-experiments/har70plus/513.csv',
+    './harth-ml-experiments/har70plus/514.csv',
+    './harth-ml-experiments/har70plus/515.csv',
+    './harth-ml-experiments/har70plus/516.csv',
+    './harth-ml-experiments/har70plus/517.csv',
+    './harth-ml-experiments/har70plus/518.csv'
+]
+
+permanent_shuffling = [19, 12, 14, 15, 1, 10, 4, 18, 5, 20, 21, 6, 11, 16, 13, 9, 7, 17, 38, 29, 33, 27, 37, 26, 40, 35, 31, 32, 25, 23, 28, 34, 2, 3, 8, 22, 24, 30, 36, 39]
+
+harth_filenames = [harth_filenames[i-1] for i in permanent_shuffling]
+train_test_split = int(0.8 * len(harth_filenames))
+
+num_windows_train = 0
+num_windows_test = 0
+
+print("Ingesting CSV files...")
+
+data_nps = [None] * len(permanent_shuffling)
+for i in range(len(permanent_shuffling)):
+    data_df = pd.read_csv(harth_filenames[i])
+    data_df.drop(['timestamp'], axis=1, inplace=True)
+    data_df.drop(['index'], axis=1, errors='ignore', inplace=True)
+    data_df.drop([''], axis=1, errors='ignore', inplace=True)
+    data_df.drop([' '], axis=1, errors='ignore', inplace=True)
+    data_df.rename({"Unnamed: 0":"a"}, errors='ignore', axis="columns", inplace=True)
+    data_df.drop(['a'], axis=1, errors='ignore', inplace=True)
+    data_df.dropna(inplace=True)
+    data_nps[i] = data_df.to_numpy(dtype=np.float32)
+    if permanent_shuffling[i]==1:
+        data_nps[i]=data_nps[i][::2]
+    if i < train_test_split:
+        num_windows_train += data_nps[i].shape[0] - num_samples + 1
+    else:
+        num_windows_test += data_nps[i].shape[0] - num_samples + 1
+    del data_df # memory is scarce after all
+    
+print("Processing windows...")
+
+train_windows = open_memmap('1-point-25-sec/train_windows_tmp.npy', mode='w+', dtype=np.float32, shape=(num_windows_train, 6, num_samples))
+train_labels = open_memmap('1-point-25-sec/train_labels_tmp.npy', mode='w+', dtype=np.int32, shape=(num_windows_train,))
+test_windows = open_memmap('1-point-25-sec/test_windows_tmp.npy', mode='w+', dtype=np.float32, shape=(num_windows_test, 6, num_samples))
+test_labels = open_memmap('1-point-25-sec/test_labels_tmp.npy', mode='w+', dtype=np.int32, shape=(num_windows_test,))
+
+num_windows_train_processed = 0
+num_windows_test_processed = 0
+
+for i in range(len(permanent_shuffling)):
+    # Since the sensors have been set to +-8G, divide by 8 to scale to 0~1 range.
+    if i < train_test_split:
+        train_windows[num_windows_train_processed:num_windows_train_processed+data_nps[i].shape[0] - num_samples + 1] = sliding_window_view(data_nps[i][:,0:6], window_shape=num_samples, axis=0)/np.float32(8)
+        train_labels[num_windows_train_processed:num_windows_train_processed+data_nps[i].shape[0] - num_samples + 1] = sp.stats.mode(sliding_window_view(data_nps[i][:,6], window_shape=num_samples), keepdims=False, axis=1)[0].astype(np.int32)
+        num_windows_train_processed += data_nps[i].shape[0] - num_samples + 1
+    else:
+        test_windows[num_windows_test_processed:num_windows_test_processed+data_nps[i].shape[0] - num_samples + 1] = sliding_window_view(data_nps[i][:,0:6], window_shape=num_samples, axis=0)/np.float32(8)
+        test_labels[num_windows_test_processed:num_windows_test_processed+data_nps[i].shape[0] - num_samples + 1] = sp.stats.mode(sliding_window_view(data_nps[i][:,6], window_shape=num_samples), keepdims=False, axis=1)[0].astype(np.int32)
+        num_windows_test_processed += data_nps[i].shape[0] - num_samples + 1
+
+del data_nps[:]
+del data_nps
+
+print("Relabelling data...")
+
+train_labels[train_labels==13] = 9
+train_labels[train_labels==14] = 10
+train_labels[train_labels==130] = 11
+train_labels[train_labels==140] = 12
+train_labels[:] = train_labels-1
+
+test_labels[test_labels==13] = 9
+test_labels[test_labels==14] = 10
+test_labels[test_labels==130] = 11
+test_labels[test_labels==140] = 12
+test_labels[:] = test_labels-1
+
+print("Saving data...")
+
+train_labels.flush()
+train_windows.flush()
+test_labels.flush()
+test_windows.flush()
+
+print("Generating class weights")
+
+# Use sklearn.utils.class_weight.compute_class_weight's algorithm without importing sklearn.
+num_classes = 12
+weights = train_labels.size/(num_classes*np.bincount(train_labels))
+class_weights = {i:weights[i] for i in range(num_classes)}
+
+with open("1-point-25-sec/class_weights.json", "w") as jsonfile:
+    json.dump(class_weights, jsonfile, indent=4)   
+    
+print("Shuffling dataset...")
+
+train_windows_shuffled = open_memmap('1-point-25-sec/train_windows.npy', mode='w+', dtype=np.float32, shape=(num_windows_train, 6, num_samples))
+train_labels_shuffled = open_memmap('1-point-25-sec/train_labels.npy', mode='w+', dtype=np.int32, shape=(num_windows_train,))
+test_windows_shuffled = open_memmap('1-point-25-sec/test_windows.npy', mode='w+', dtype=np.float32, shape=(num_windows_test, 6, num_samples))
+test_labels_shuffled = open_memmap('1-point-25-sec/test_labels.npy', mode='w+', dtype=np.int32, shape=(num_windows_test,))
+
+train_shuffle = np.arange(train_labels.size)
+test_shuffle = np.arange(test_labels.size)
+np.random.shuffle(train_shuffle)
+np.random.shuffle(test_shuffle)
+
+for i in range(train_labels.size):
+    train_windows_shuffled[i] = train_windows[train_shuffle[i]]
+    train_labels_shuffled[i] = train_labels[train_shuffle[i]]
+    
+for i in range(test_labels.size):
+    test_windows_shuffled[i] = test_windows[test_shuffle[i]]
+    test_labels_shuffled[i] = test_labels[test_shuffle[i]]
+
+print("Flush...")
+
+train_windows_shuffled.flush()
+train_labels_shuffled.flush()
+test_windows_shuffled.flush()
+test_labels_shuffled.flush()
+
+print("Preprocessing Complete!")
